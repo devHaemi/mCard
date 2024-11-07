b7b42ee5e341afdcd30e040830d8e10bcf6f73bc
hmJeon <hmjeon@aiblab.co.kr>
2024-11-07 17:33:12 +0900

    카드리스트 데이터 준비
    - react-router-dom 설치
    - mock 데이터 firebase 올리기

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,16 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Button from '@/components/shared/Button'
-import Text from '@/components/shared/Text'
-import TextField from '@/components/shared/TextField'
-import { useAlertContext } from '@/contexts/AlertContext'
+import HomePage from '@/pages/Home'
+import TestPage from '@/pages/Test'
 
 function App() {
-  const { open } = useAlertContext()
-
   return (
-    <>
-      <Text display="block" typography="t1" color="red">
-        t1
-      </Text>
-      <Text display="block" typography="t2" color="blue">
-        t2
-      </Text>
-      <Text display="block" typography="t3">
-        t3
-      </Text>
-      <Text display="block" typography="t4">
-        t4
-      </Text>
-      <Text display="block" typography="t5">
-        t5
-      </Text>
-
-      <Button>button</Button>
-      <Button weak>button</Button>
-      <Button color="success">button</Button>
-      <Button color="success" weak>
-        button
-      </Button>
-      <Button color="error">button</Button>
-      <Button color="error" weak>
-        button
-      </Button>
-      <Button full>button</Button>
-      <Button full disabled>
-        button
-      </Button>
-
-      <TextField label="아이디" />
-      <TextField
-        label="비밀번호"
-        hasError={true}
-        helpMessage="비밀번호가 맞지 않습니다."
-      />
-
-      <Button
-        onClick={() => {
-          open({
-            title: '카드신청완료',
-            description: '내역페이지에서 확인하세요.',
-            onButtonClick: () => {
-              //
-            },
-          })
-        }}
-      >
-        Alert Open
-      </Button>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={HomePage} />
+        <Route path="/test" Component={TestPage} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
