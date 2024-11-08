1830516c0f90dd9eeb161919a9d6bf6f1f5eb928
hmJeon <hmjeon@aiblab.co.kr>
2024-11-08 11:38:39 +0900

    인피니티 스크롤 구현

--- src/components/home/CardList.tsx
+++ src/components/home/CardList.tsx
@@ -1,32 +1,71 @@
-import { useQuery } from 'react-query'
+import { useInfiniteQuery } from 'react-query'
+import { useNavigate } from 'react-router-dom'
+import { flatten } from 'lodash'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
-import ListRow from '@/components/shared/ListRow'
 import { getCards } from '@/remote/card'
+import ListRow from '@/components/shared/ListRow'
+import { useCallback } from 'react'
 
 function CardList() {
-  const { data } = useQuery(['cards'], () => getCards())
+  const {
+    data,
+    hasNextPage = false,
+    fetchNextPage,
+    isFetching,
+  } = useInfiniteQuery(
+    ['cards'],
+    ({ pageParam }) => {
+      return getCards(pageParam)
+    },
+    {
+      getNextPageParam: (snapshot) => {
+        return snapshot.lastVisible
+      },
+    },
+  )
+
+  const navigate = useNavigate()
+
+  const loadMore = useCallback(() => {
+    if (hasNextPage === false || isFetching) {
+      return
+    }
+
+    fetchNextPage()
+  }, [fetchNextPage, hasNextPage, isFetching])
 
   if (data == null) {
     return null
   }
 
+  const cards = flatten(data?.pages.map(({ items }) => items))
+
   return (
-    <div>
+    <InfiniteScroll
+      dataLength={cards.length}
+      hasMore={hasNextPage}
+      loader={<></>}
+      next={loadMore}
+    >
       <ul>
-        {data.map((card, index) => {
+        {cards.map((card, index) => {
           return (
             <ListRow
               key={card.id}
               contents={
                 <ListRow.Texts title={`${index + 1}위`} subTitle={card.name} />
               }
-              right={card.payback != null ? <div>{card.payback}</div> : null}
+              right={card.payback != null ? <div>{card.payback} </div> : null}
               withArrow={true}
+              onClick={() => {
+                navigate(`/card/${card.id}`)
+              }}
             />
           )
         })}
       </ul>
-    </div>
+    </InfiniteScroll>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -14,14 +14,16 @@ const client = new QueryClient({
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
     <Global styles={globalStyles} />
     <QueryClientProvider client={client}>
       <AlertContextProvider>
         <App />
       </AlertContextProvider>
     </QueryClientProvider>
-  </React.StrictMode>,
+  </>,
+  // </React.StrictMode>,
 )
 
 // If you want to start measuring performance in your app, pass a function
