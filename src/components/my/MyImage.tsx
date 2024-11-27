c373a0c94da642e3c17d476323605578344f4bd3
hmJeon <hmjeon@aiblab.co.kr>
2024-11-27 22:23:56 +0900

    MyPage UI 생성

---
+++ src/components/my/MyImage.tsx
@@ -0,0 +1,55 @@
+import useUser from '@/hooks/auth/useUser'
+import styled from '@emotion/styled'
+
+function MyImage({
+  size = 40,
+  mode = 'default',
+}: {
+  size?: number
+  mode?: 'default' | 'upload'
+}) {
+  const user = useUser()
+
+  const handleUploadImage = () => {
+    //
+  }
+
+  return (
+    <Container>
+      <img
+        src={
+          user?.photoURL ||
+          'https://cdn1.iconfinder.com/data/icons/user-pictures/100/male3-64.png'
+        }
+        alt="유저의 이미지"
+        width={size}
+        height={size}
+      />
+      {mode === 'upload' ? (
+        <input type="file" accept="image/*" onChange={handleUploadImage} />
+      ) : null}
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+
+  & img {
+    border-radius: 100%;
+  }
+
+  & input[type='file'] {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    cursor: pointer;
+  }
+`
+
+export default MyImage
